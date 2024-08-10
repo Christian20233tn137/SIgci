f35d57c634ba1c0dd23906c2a962b48d8787fb2b
isaacOG145 <20233tn145@utez.edu.mx>
2024-08-10 17:57:36 -0600

    clase para el filtro

---
+++ Sigci/Sigci/src/main/java/utez/edu/mx/sicci/utils/AdminFilter.java
@@ -0,0 +1,35 @@
+package utez.edu.mx.sicci.utils;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import utez.edu.mx.sicci.model.User;
+
+import java.io.IOException;
+
+public class AdminFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        HttpSession session = httpRequest.getSession(false);
+        User user = (session != null) ? (User) session.getAttribute("user") : null;
+
+        if (user != null && user.getIdtipo_usuario() == 1) { // Verifica si el usuario es administrador
+            chain.doFilter(request, response); // Permite el acceso
+        } else {
+            httpResponse.sendRedirect("index.jsp"); // Redirige a la página de no entrar en el perfil
+        }
+    }
+
+    public void destroy() {
+
+    }
+}
